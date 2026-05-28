20fcd8b051cfdaff98f74e5770a8ac38df637d1c
Nicoleta Nhamuave <nicoletanhamuave27@gmail.com>
2026-05-28 10:10:38 +0200

    Trocas de Sambo e Nhamuave no projecto que s=shannon iniciou

---
+++ POO.JAVA-main/src/model/Exame.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Exame {
+    private String tipo;
+    private String idExame;
+    private String data;
+    private String resultado;
+    private String observacoes;
+    private Paciente paciente;
+
+    
+
+    public Exame(String tipo, String idExame, String data, String resultado, String observacoes, Paciente paciente) {
+        this.tipo = tipo;
+        this.idExame = idExame;
+        this.data = data;
+        this.resultado = resultado;
+        this.observacoes = observacoes;
+        this.paciente = paciente;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public String getData() {
+        return data;
+    }
+    public String getResultado() {
+        return resultado;
+    }
+    public String getObservacoes() {
+        return observacoes;
+    }
+    public Paciente getPaciente() {
+        return paciente;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    
+    
+}
